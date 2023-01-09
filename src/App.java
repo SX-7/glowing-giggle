e9d5ef60ad4566c5fa5a94f3845f33cf71e5ea2e
SX_7 <sn1.test.preria.2002@gmail.com>
2023-01-09 13:08:20 +0100

    Added properties parsing Split some lines

--- src/App.java
+++ src/App.java
@@ -11,12 +11,13 @@ import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Map.Entry;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        //Start the logger
-        final Logger logger = LogManager.getLogger(App.class.getName()); //TODO: logging points
-        //logger.trace("Logger initialized, app started");
-        //Get the pages to download
+        // Start the logger
+        final Logger logger = LogManager.getLogger(App.class.getName()); // TODO: logging points
+        // logger.trace("Logger initialized, app started");
+        // Get the pages to download
 
         String configFilePath = new String("config.properties");
         Properties props = new Properties();
@@ -24,35 +25,43 @@ public class App {
         try (InputStream is = new FileInputStream(propsFile)) {
             props.loadFromXML(is);
         } catch (Exception e) {
-            //logger.fatal("Error while loading the 'config.properties' file, aborting program.",e);
+            // logger.fatal("Error while loading the 'config.properties' file, aborting
+            // program.",e);
             System.exit(1);
         }
-        
-        //save properties into some property-holder class?
+
+        // save properties into some property-holder class?
         HashSet<ArticleSourceProperties> ArticleMatchers = new HashSet<ArticleSourceProperties>();
         for (Entry<Object, Object> property : props.entrySet()) {
-            ArticleMatchers.add(new ArticleSourceProperties(property.getKey().toString(), property.getValue().toString()));
+            ArticleMatchers
+                    .add(new ArticleSourceProperties(property.getKey().toString(), property.getValue().toString()));
         }
-        
-        //iterate over properties to scrape proper things
-        //preferably save data in some article-holder class
+
+        // iterate over properties to scrape proper things
+        // preferably save data in some article-holder class
         HashSet<Article> articles = new HashSet<Article>();
         for (ArticleSourceProperties articleProps : ArticleMatchers) {
             Document document = Jsoup.connect(articleProps.getPageURL()).get();
             Elements articlesScraped = document.select(articleProps.getContainerMatcher());
             for (Element articleScraped : articlesScraped) {
-                    Article ar = new Article(articleScraped.select(articleProps.getUrlMatcher()).first().attr("abs:href"),articleScraped.select(articleProps.getTitleMatcher()).first().text());
-                    //TODO: handling empty entries
+                Article ar = new Article(articleScraped.select(articleProps.getUrlMatcher()).first().attr("abs:href"),
+                        articleScraped.select(articleProps.getTitleMatcher()).first().text());
+                if (!articleProps.getAuthorMatcher().isBlank()) {
                     ar.articleAuthor = articleScraped.select(articleProps.getAuthorMatcher()).first().text();
+                }
+                if (!articleProps.getSubTitleMatcher().isBlank()) {
                     ar.articleSubTitle = articleScraped.select(articleProps.getSubTitleMatcher()).first().text();
+                }
+                if (!articleProps.getDateMatcher().isBlank()) {
                     ar.articleDate = articleScraped.select(articleProps.getDateMatcher()).first().text();
+                }
+                if (!articleProps.getArticleMatcher().isBlank()) {
                     Document article = Jsoup.connect(ar.articleURL).get();
-                    ar.articleContent = article.select(articleProps.getArticleMatcher()).text(); //TODO: check if this is proper at all
-                    articles.add(ar);
+                    ar.articleContent = article.select(articleProps.getArticleMatcher()).text();    
+                }
+                articles.add(ar);
             }
         }
-        
-        
-        
+
     }
 }
--- src/ArticleSourceProperties.java
+++ src/ArticleSourceProperties.java
@@ -1,48 +1,80 @@
 public class ArticleSourceProperties {
 
     private String pageURL = null;
+
     public String getPageURL() {
         return pageURL;
     }
 
     private String containerMatcher = null;
+
     public String getContainerMatcher() {
         return containerMatcher;
     }
 
     private String urlMatcher = null;
+
     public String getUrlMatcher() {
         return urlMatcher;
     }
 
     private String titleMatcher = null;
+
     public String getTitleMatcher() {
         return titleMatcher;
     }
 
     private String subTitleMatcher = null;
+
     public String getSubTitleMatcher() {
         return subTitleMatcher;
     }
 
     private String authorMatcher = null;
+
     public String getAuthorMatcher() {
         return authorMatcher;
     }
 
     private String dateMatcher = null;
+
     public String getDateMatcher() {
         return dateMatcher;
     }
 
     private String articleMatcher = null;
+
     public String getArticleMatcher() {
         return articleMatcher;
     }
 
     public ArticleSourceProperties(String webpageURL, String matchersToDisassambly) {
-        this.pageURL=webpageURL;
-        //TODO: splitting of the matchers, error handling - or even exception throwing + escalation
+        this.pageURL = webpageURL;
+        String dissasembledMatchers[] = matchersToDisassambly.split(";");
+        this.containerMatcher = dissasembledMatchers[0];
+        this.urlMatcher = dissasembledMatchers[1];
+        this.titleMatcher = dissasembledMatchers[2]; // these three have to exist
+        // these below don't need to
+        if (dissasembledMatchers.length > 3) {
+            if (!dissasembledMatchers[3].isBlank()) {
+                this.subTitleMatcher = dissasembledMatchers[3];
+            }
+        }
+        if (dissasembledMatchers.length > 4) {
+            if (!dissasembledMatchers[4].isBlank()) {
+                this.authorMatcher = dissasembledMatchers[4];
+            }
+        }
+        if (dissasembledMatchers.length > 5) {
+            if (!dissasembledMatchers[5].isBlank()) {
+                this.dateMatcher = dissasembledMatchers[5];
+            }
+        }
+        if (dissasembledMatchers.length > 6) {
+            if (!dissasembledMatchers[6].isBlank()) {
+                this.articleMatcher = dissasembledMatchers[6];
+            }
+        }
     }
-    
+
 }
